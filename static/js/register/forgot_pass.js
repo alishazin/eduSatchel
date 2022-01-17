3b2d4f9091c4f441533e02e6265c9c3c6eaea4fc
alishazin <alishazin100@gmail.com>
2022-01-17 10:07:41 +0530

    forgot-password frontend over

--- static/js/register/forgot_pass.js
+++ static/js/register/forgot_pass.js
@@ -1,3 +1,29 @@
 function onLoad() {
-    console.log('initialized');
+    addClickEventToField();
+}
+
+function addClickEventToField() {
+    const inputField = Array.from(document.querySelectorAll('.parent > form > .field-parent > input'));
+    inputField.forEach((field) => {
+        // calling it at the start if contains value and adding it as onfocus callback
+        const onFocusFunc = function () {
+            const label = field.parentElement.children[1];
+            label.style.top = '-8px';
+            label.style.fontSize = '13px';
+        };
+        // made asynchronous for giving chrome time to add former value
+        setTimeout(() => {
+            if (field.value.length > 0) {
+                onFocusFunc();
+            }
+        }, 100) 
+        field.onfocus = onFocusFunc;
+        field.onblur = () => {
+            if (field.value.length === 0) {
+                const label = field.parentElement.children[1];
+                label.style.top = '12.7px';
+                label.style.fontSize = '14px';
+            }
+        }
+    })
 }
