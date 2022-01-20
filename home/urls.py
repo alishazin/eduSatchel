750136a25cc9673817bcc6aaf6404337fe358e4b
alishazin <alishazin100@gmail.com>
2022-01-20 13:26:03 +0530

    home base completed

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from .views import test_base
 
 app_name = 'home'
 
 urlpatterns = [
-    
+    path('', test_base)
 ]
---
+++ static/js/home/base.js
@@ -0,0 +1,27 @@
+function onBaseLoad() {
+    addHoverToNavBarContentBox();
+}
+
+function addHoverToNavBarContentBox() {
+    const notSelectedBox = document.querySelector('body > .nav-bar > .content-box:not(.selected)');
+    const icon = document.querySelector('body > .nav-bar > .content-box:not(.selected) > i');
+    const span = document.querySelector('body > .nav-bar > .content-box:not(.selected) > span');
+    notSelectedBox.onmouseover = () => {
+        if (notSelectedBox.id === 'profile-box') {
+            span.style.transform = 'translateX(48px)';
+            span.style.opacity = '1';
+        } else {
+            span.style.transform = 'translateX(-5px) translateY(-1px)';
+            icon.style.transform = 'translateX(-48px)';
+            span.style.opacity = '1';
+        }
+    }
+    notSelectedBox.onmouseleave = () => {
+        if (notSelectedBox.id === 'profile-box') {
+            span.style.opacity = '0';
+        } else {
+            span.style.opacity = '0';
+            icon.style.transform = 'translateX(0)';
+        }
+    }
+}
