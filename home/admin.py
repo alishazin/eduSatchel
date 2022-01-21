24e02662eda37495499d380a54befc4e3c68ce92
alishazin <alishazin100@gmail.com>
2022-01-21 17:31:16 +0530

    added CLASS Model

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Class
 
 # Register your models here.
+
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ('id', 'teacher', 'title', 'date_started', 'state') 
+    readonly_fields = ('id', 'date_started')
+
+admin.site.register(Class, ClassAdmin)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from register.models import CustomUser
+
+import uuid
 
 # Create your models here.
+
+class Class(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    teacher = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    # cascade will be changed in the future
+    title = models.CharField(max_length=80, null=False, blank=False)
+    description = models.CharField(max_length=300, null=False, blank=False)
+    date_started = models.DateField(auto_now_add=True)
+    state = models.BooleanField(default=True)
+    # True means can enter, False means closed
+
