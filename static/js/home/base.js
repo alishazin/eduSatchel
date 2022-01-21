b02f569eab3e64862396f82eacbc85b38cbd8781
alishazin <alishazin100@gmail.com>
2022-01-21 22:57:32 +0530

    completed nav bar with notification icon and counter

--- static/js/home/base.js
+++ static/js/home/base.js
@@ -3,52 +3,40 @@ function onBaseLoad() {
     addClickToNavBarContentBox();
 }
 
-// function addHoverToNavBarContentBox() {
-//     const notSelectedBox = Array.from(document.querySelector('body > .nav-bar > .content-box:not(.selected)'));
-//     notSelectedBox.forEach((self) => {
-//         const icon = self.children[0];
-//         const span = self.children[1];
-//         self.onmouseover = () => {
-//             if (self.id === 'profile-box') {
-//                 span.style.transform = 'translateX(48px)';
-//                 span.style.opacity = '1';
-//             } else if (self.id === 'notification-box') {
-//                 span.style.transform = 'translateX(-5px) translateY(-1px)';
-//                 icon.style.transform = 'translateX(-48px)';
-//                 span.style.opacity = '1';
-//             }
-//         }
-//         self.onmouseleave = () => {
-//             if (self.id === 'notification-box') {
-//                 span.style.opacity = '0';
-//             } else if (self.id === 'home-box') {
-//                 span.style.opacity = '0';
-//                 icon.style.transform = 'translateX(0)';
-//             }
-//         }
-//     })
-// }
 function addHoverToNavBarContentBox() {
-    const notSelectedBox = Array.from(document.querySelector('body > .nav-bar > .content-box:not(.selected)'));
+    const notSelectedBox = Array.from(document.querySelectorAll('body > .nav-bar > .content-box:not(.selected)'));
     notSelectedBox.forEach((self) => {
         const icon = self.children[0];
         const span = self.children[1];
         self.onmouseover = () => {
-            if (self.id === 'profile-box') {
-                span.style.transform = 'translateX(48px)';
-                span.style.opacity = '1';
-            } else if (self.id === 'notification-box') {
-                span.style.transform = 'translateX(-5px) translateY(-1px)';
-                icon.style.transform = 'translateX(-48px)';
-                span.style.opacity = '1';
+            if (window.innerWidth > 450) {
+                if (self.id === 'notification-box') {
+                    span.style.transform = 'translateX(68px)';
+                    span.style.opacity = '1';
+                } else if (self.id === 'home-box') {
+                    span.style.transform = 'translateX(-5px) translateY(-1px)';
+                    icon.style.transform = 'translateX(-48px)';
+                    span.style.opacity = '1';
+                } else if (self.id === 'profile-box') {
+                    self.style.flexBasis = '130px';
+                    span.style.transform = 'translateX(17px)';
+                    icon.style.transform = 'translateX(-30px)';
+                    span.style.opacity = '1';
+                }
             }
         }
         self.onmouseleave = () => {
-            if (self.id === 'notification-box') {
-                span.style.opacity = '0';
-            } else if (self.id === 'home-box') {
-                span.style.opacity = '0';
-                icon.style.transform = 'translateX(0)';
+            if (window.innerWidth > 450) {
+                if (self.id === 'notification-box') {
+                    span.style.opacity = '0';
+                } else if (self.id === 'home-box') {
+                    span.style.opacity = '0';
+                    icon.style.transform = 'translateX(0)';
+                } else if (self.id === 'profile-box') {
+                    self.style.flexBasis = '50px';
+                    icon.style.transform = 'translateX(0)';
+                    span.style.opacity = '0';
+                }
             }
         }
     })
