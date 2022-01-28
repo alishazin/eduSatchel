8b588cb686444dfa26c0c77db2708ff6c3e4a5da
alishazin <alishazin100@gmail.com>
2022-01-28 21:04:47 +0530

    completed teacher detail box

--- classmenu/views.py
+++ classmenu/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from edusatchel.decorators import classentry_check
+from home.models import Class
 # from home.views import AccountTypeView
 
 # Create your views here.
@@ -9,5 +10,5 @@ class ClassMenuView(View):
     @classentry_check()
     def get(self, request, classID):
         return render(request, 'classmenu/class.html', {
-            'classID' : classID,
+            'classObj' : Class.objects.get(id=classID),
         })
--- static/js/classmenu/class.js
+++ static/js/classmenu/class.js
@@ -1,5 +1,34 @@
+
+var teacherDetailBox = {};
+
 function onLoad() {
     navBarObj.selectItem(1);
+
+    teacherDetailBox = {
+        parent : document.querySelector('body > .parent-content > .right-content > .teacher-details'),
+        extendButt : document.querySelector('body > .parent-content > .right-content > .teacher-details > .extend-div'),
+        icon : document.querySelector('body > .parent-content > .right-content > .teacher-details > .extend-div > i'),
+        _state : false,
+        get state() {
+            return this._state
+        },
+        set state(arg) {
+            if (arg === true) {
+                this.parent.style.height = '525px'; 
+                this.icon.style.transform = 'rotate(180deg)';
+            } else if (arg === false) {
+                this.parent.style.height = '300px'; 
+                this.icon.style.transform = 'rotate(0)';
+            }
+            this._state = arg;
+        },
+        addCallbacks : function () {
+            this.extendButt.onclick = () => {
+                this.state = this.state === true ? false : true;
+            }
+        }
+    };
+    teacherDetailBox.addCallbacks();
 
     addEventToCopyClassID();
 }
