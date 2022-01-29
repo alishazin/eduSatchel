582d910c34db8c7fdfcc94158050de59f89bcb0f
alishazin <alishazin100@gmail.com>
2022-01-29 14:29:56 +0530

    added file and messagePublic models

--- classmenu/admin.py
+++ classmenu/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import File, MessagePublic
 
 # Register your models here.
+
+class FileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'location_hint', 'class_obj') 
+    readonly_fields = ('id',)
+
+class MessagePublicAdmin(admin.ModelAdmin):
+    list_display = ('id', 'content', 'class_obj', 'user') 
+    readonly_fields = ('id',)
+
+admin.site.register(File, FileAdmin)
+admin.site.register(MessagePublic, MessagePublicAdmin)
--- classmenu/models.py
+++ classmenu/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from home.models import Class
+from register.models import CustomUser
+
+import uuid
+
+def get_file_upload_location(self, filename):
+    return f"files/{self.class_obj.id}/{self.location_hint}/{uuid.uuid4()}/{filename}"
+
+LOCATION_HINTS = (
+    ('public', 'Public'),
+)
 
 # Create your models here.
+
+class File(models.Model):
+    file = models.FileField(upload_to=get_file_upload_location, max_length=500)
+    location_hint = models.CharField(max_length=10, choices=LOCATION_HINTS, blank=False, null=False)
+    class_obj = models.ForeignKey(Class, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'<{self.location_hint}> of <{self.class_obj}>'
+
+class MessagePublic(models.Model):
+    content = models.CharField(max_length=300, blank=False, null=False)
+    class_obj = models.ForeignKey(Class, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    files = models.ManyToManyField(File)
+    # add() to add to files
+    # set() to set new querysets of files
+
+    def __str__(self):
+        return f'<{self.content}> from <{self.user}>'
