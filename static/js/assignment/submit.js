6338637f319da04698be87b885e9e6917513b41e
alishazin <alishazin100@gmail.com>
2022-02-17 18:16:21 +0530

    frontend design over for submission

--- static/js/assignment/submit.js
+++ static/js/assignment/submit.js
@@ -1,4 +1,24 @@
 
+var formObject = {}
+
 function onLoad() {
     navBarObj.selectItem(1);
+
+    initializeFormObject();
+
+    return document.querySelector('.content-box > .error-div');
+}
+
+function initializeFormObject() {
+    formObject = {
+        messageField : document.querySelector('.content-box > textarea'),
+        addCallbacks : function () {
+            this.messageField.oninput = () => {
+                if (this.messageField.value.trim()) { this.messageField.className = 'inputted'; } 
+                else { this.messageField.className = ''; }
+            }
+        }
+    }
+    formObject.addCallbacks()
+
 }
