00f2e48e18708bbceaf8f4cd14b21a7de1fea2dd
alishazin <78612608+alishazin@users.noreply.github.com>
2023-03-02 19:04:22 +0530

    added -admin-access-code

--- register/rest_view.py
+++ register/rest_view.py
@@ -4,33 +4,40 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from .models import CustomUser
+from edusatchel.settings import ADMIN_ACCES_CODE
 
 @method_decorator(csrf_exempt, name='dispatch')
 class CreateTeacherAccountView(View):
 
     def post(self, request):
-        user_obj = CustomUser.objects.create_user(
-            portal_id = request.POST['portal_id'],
-            email = request.POST['email'],
-            username = request.POST['username'],
-            password = request.POST['password'],
-            account_type = 'teacher'
-        )
-        user_obj.profile_pic = request.POST['profile_pic']
-        user_obj.save()
-        return HttpResponse("sadasd")
+        if "HTTP_ADMIN_ACCESS_CODE" in request.META and request.META["HTTP_ADMIN_ACCESS_CODE"] == ADMIN_ACCES_CODE:
+            user_obj = CustomUser.objects.create_user(
+                portal_id = request.POST['portal_id'],
+                email = request.POST['email'],
+                username = request.POST['username'],
+                password = request.POST['password'],
+                account_type = 'teacher'
+            )
+            user_obj.profile_pic = request.POST['profile_pic']
+            user_obj.save()
+            return HttpResponse("ok")
+        
+        return HttpResponse("not ok")
 
 @method_decorator(csrf_exempt, name='dispatch')
 class CreateStudentAccountView(View):
 
     def post(self, request):
-        user_obj = CustomUser.objects.create_user(
-            portal_id = request.POST['portal_id'],
-            email = request.POST['email'],
-            username = request.POST['username'],
-            password = request.POST['password'],
-            account_type = 'student'
-        )
-        user_obj.profile_pic = request.POST['profile_pic']
-        user_obj.save()
-        return HttpResponse("sadasd")
+        if "HTTP_ADMIN_ACCESS_CODE" in request.META and request.META["HTTP_ADMIN_ACCESS_CODE"] == ADMIN_ACCES_CODE:
+            user_obj = CustomUser.objects.create_user(
+                portal_id = request.POST['portal_id'],
+                email = request.POST['email'],
+                username = request.POST['username'],
+                password = request.POST['password'],
+                account_type = 'student'
+            )
+            user_obj.profile_pic = request.POST['profile_pic']
+            user_obj.save()
+            return HttpResponse("ok")
+        
+        return HttpResponse("not ok")
